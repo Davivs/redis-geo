b91e12e4d7538f360690eda2a7ddb3411823acce
Andre <wowunroxx@gmail.com>
2019-09-01 23:15:12 +0100

    trigger trevis

--- .history/deploy-envs_20190901225715.sh
+++ .history/deploy-envs_20190901225715.sh
@@ -8,7 +8,6 @@ export AWS_DEFAULT_REGION=us-east-1b
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=3.226.227.87
 
-
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 # optional: SERVICESTACK_LICENSE
